ecf0ebb06d1a9d4b2b22924a5be821a23d526136
dtrung98 <trung17dinhle@gmail.com>
2019-07-17 18:25:32 +0700

    language

--- app/src/main/java/com/ldt/musicr/ui/BaseActivity.java
+++ app/src/main/java/com/ldt/musicr/ui/BaseActivity.java
@@ -1,18 +1,22 @@
 package com.ldt.musicr.ui;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.AudioManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
@@ -22,6 +26,7 @@ import android.support.v7.graphics.Palette;
 import android.util.Log;
 
 
+import com.ldt.musicr.helper.LocaleHelper;
 import com.ldt.musicr.helper.songpreview.SongPreviewController;
 import com.ldt.musicr.loader.PaletteGeneratorTask;
 import com.ldt.musicr.service.MusicPlayerRemote;
@@ -38,6 +43,7 @@ import com.squareup.picasso.Picasso;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static com.ldt.musicr.util.BitmapEditor.updateSat;
 
@@ -295,6 +301,37 @@ public void addMusicServiceEventListener(final MusicServiceEventListener listene
     }
 
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(updateBaseContextLocale(base));
+    }
 
+    private Context updateBaseContextLocale(Context context) {
+        String language = LocaleHelper.getLanguage(context); // Helper method to get saved language from SharedPreferences
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return updateResourcesLocale(context, locale);
+        }
+
+        return updateResourcesLocaleLegacy(context, locale);
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private Context updateResourcesLocale(Context context, Locale locale) {
+        Configuration configuration = context.getResources().getConfiguration();
+        configuration.setLocale(locale);
+        return context.createConfigurationContext(configuration);
+    }
+
+    @SuppressWarnings("deprecation")
+    private Context updateResourcesLocaleLegacy(Context context, Locale locale) {
+        Resources resources = context.getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.locale = locale;
+        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+        return context;
+    }
 
 }
--- app/src/main/java/com/ldt/musicr/ui/bottomnavigationtab/setting/SettingTabFragment.java
+++ app/src/main/java/com/ldt/musicr/ui/bottomnavigationtab/setting/SettingTabFragment.java
@@ -1,17 +1,43 @@
 package com.ldt.musicr.ui.bottomnavigationtab.setting;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ldt.musicr.R;
+import com.ldt.musicr.helper.LocaleHelper;
 import com.ldt.musicr.ui.widget.fragmentnavigationcontroller.SupportFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import es.dmoral.toasty.Toasty;
 
 public class SettingTabFragment extends SupportFragment {
     @Nullable
     @Override
     protected View onCreateView(LayoutInflater inflater, ViewGroup container) {
         return inflater.inflate(R.layout.setting_tab_fragment,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this,view);
+    }
+
+    @OnClick(R.id.button)
+    void doSomething() {
+        Activity activity = getActivity();
+        if(activity!=null) {
+            Toasty.normal(activity,"Clicked").show();
+            String lang = LocaleHelper.getLanguage(activity);
+            LocaleHelper.setLocale(activity, lang.equals("vi") ?"en" : "vi");
+           activity.recreate();
+        }
     }
 }
