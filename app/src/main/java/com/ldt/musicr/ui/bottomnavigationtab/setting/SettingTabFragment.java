d2dd613931165a19ccb4a47008c64690747cf8ee
dtrung98 <trung17dinhle@gmail.com>
2019-07-22 17:52:53 +0700

    no changed

--- app/src/main/java/com/ldt/musicr/ui/bottomnavigationtab/setting/SettingTabFragment.java
+++ app/src/main/java/com/ldt/musicr/ui/bottomnavigationtab/setting/SettingTabFragment.java
@@ -1,6 +1,7 @@
 package com.ldt.musicr.ui.bottomnavigationtab.setting;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
