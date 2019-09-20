d3b45d0ff21ce22f7d90ca4a19b6779d25068a3c
dtrung98 <trung17dinhle@gmail.com>
2019-09-20 18:59:23 +0700

    bubble genres

--- app/src/main/java/com/ldt/musicr/ui/page/librarypage/GenreChildTab.java
+++ app/src/main/java/com/ldt/musicr/ui/page/librarypage/GenreChildTab.java
@@ -1,20 +1,37 @@
 package com.ldt.musicr.ui.page.librarypage;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.ldt.bubblepicker.BubblePickerListener;
+import com.ldt.bubblepicker.adapter.BubblePickerAdapter;
+import com.ldt.bubblepicker.model.BubbleGradient;
+import com.ldt.bubblepicker.model.PickerItem;
+import com.ldt.bubblepicker.rendering.BubblePicker;
+import com.ldt.bubblepicker.rendering.Item;
+import com.ldt.bubblepicker.rendering.java.gltexture.TextureBubblePicker;
 import com.ldt.musicr.R;
+import com.ldt.musicr.loader.medialoader.GenreLoader;
 import com.ldt.musicr.loader.medialoader.SongLoader;
 import com.ldt.musicr.model.Artist;
+import com.ldt.musicr.model.Genre;
 import com.ldt.musicr.model.Song;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
@@ -26,20 +43,30 @@ import butterknife.ButterKnife;
 
 public class GenreChildTab extends Fragment {
     public static final String TAG="GenreChildTab";
-    // we need these very low values to make sure our artist image loading calls doesn't block the image loading queue
-    private static final int TIMEOUT = 750;
 
-    private static final String ARTIST = "artist";
+    private final static String ROBOTO_BOLD = "roboto_bold.ttf";
+    private final static String ROBOTO_MEDIUM = "roboto_medium.ttf";
+    private final static String ROBOTO_REGULAR = "roboto_regular.ttf";
 
-    public static GenreChildTab newInstance(Artist artist) {
+    public Typeface mBoldTypeface;
+    public Typeface mMediumTypeface;
+    public Typeface mRegularTypeface;
 
-        Bundle args = new Bundle();
-        if(artist!=null)
-            args.putParcelable(ARTIST,artist);
+    private void initTypefaces() {
+        Context context = getContext();
+        if(context !=null) {
+            AssetManager assets = context.getAssets();
+            mBoldTypeface = Typeface.createFromAsset(assets,ROBOTO_BOLD);
+            mMediumTypeface = Typeface.createFromAsset(assets,ROBOTO_MEDIUM);
+            mRegularTypeface = Typeface.createFromAsset(assets,ROBOTO_REGULAR);
+        }
+    }
 
-        GenreChildTab fragment = new GenreChildTab();
-        fragment.setArguments(args);
-        return fragment;
+    @BindView(R.id.bubble_picker)
+    TextureBubblePicker mBubblePicker;
+
+    public static GenreChildTab newInstance() {
+        return new GenreChildTab();
     }
 
     @Nullable
@@ -52,110 +79,69 @@ public class GenreChildTab extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this,view);
-        mSwipeRefresh.setOnRefreshListener(this::refresh);
+        initTypefaces();
+        initBubblePicker();
     }
 
-    @BindView(R.id.swipe_refresh) SwipeRefreshLayout mSwipeRefresh ;
-    private void refresh() {
-    new TestingTask(this).execute();
+    private BubblePickerAdapter mAdapter;
+    private ArrayList<Genre> mGenres = new ArrayList<>();
+
+    public void refreshData() {
+        if(getContext()!=null) {
+            mGenres.clear();
+            mGenres.addAll(GenreLoader.getAllGenres(getContext()));
+        }
     }
 
-    private void test() {
-        if(getContext()==null) return;
-        HashMap<UserKey, Song> hash = new HashMap<>();
-        ArrayList<Song> ori = SongLoader.getAllSongs(getContext());
+    public void initBubblePicker() {
+        if(mBubblePicker==null) return;
+        refreshData();
 
-        ArrayList<Song> list = new ArrayList<>(ori);
+        String[] titles = getResources().getStringArray(R.array.countries);
 
-        for (Song song:
-             list) {
-            hash.put(new UserKey(song.title,song.id),song);
-        }
+        TypedArray colors = getResources().obtainTypedArray(R.array.colors);
 
+        mBubblePicker.setBubbleSize(18);
+        Item.Companion.setBitmapSize(128f);
+        Item.Companion.setTextSizeRatio(40f/280);
 
-        Random random = new Random();
-        Song[] songs = new Song[ori.size()];
-        for (int i = 0; !ori.isEmpty(); i++) {
-           songs[i] = ori.remove(random.nextInt(ori.size()));
-        }
+        mAdapter = new BubblePickerAdapter() {
 
-        int result_one = 0;
-        int result_two = 0;
-        String one = "";
-
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 10; i++) {
-
-            for (Song song :
-                    songs) {
-                Song s = hash.get(new UserKey(song.title, song.id));
-                if(i==0) one +=s.title+"\n";
-                if (s != null) result_one++;
-            }
-        }
-
-        long time_one = System.currentTimeMillis();
-
-        for (int i = 0; i < 10; i++) {
-
-            for (Song song : songs) {
-                int index = list.indexOf(song);
-                if (index != -1) {
-                    Song s = list.get(index);
-                    if (s != null)
-                        result_two++;
-                }
-            }
-        }
-        long time_two = System.currentTimeMillis();
-        Log.d(TAG, "test: total size = "+ songs.length);
-        Log.d(TAG, "test: hash = "+(time_one- start)+", found = "+result_one);
-        Log.d(TAG, "test: list = "+(time_two-time_one)+", found = "+result_two);
-        Log.d(TAG, "test: \n"+one);
-
-        mSwipeRefresh.post(new Runnable() {
             @Override
-            public void run() {
-                mSwipeRefresh.setRefreshing(false);
+            public int getTotalCount() {
+                return mGenres.size();
             }
+
+            @Override
+            @NonNull
+            public PickerItem getItem(int position) {
+                PickerItem item = new PickerItem();
+                item.setTitle(mGenres.get(position).name);
+                item.setGradient( new BubbleGradient(colors.getColor((position * 2) % 8, 0),
+                            colors.getColor((position * 2) % 8 + 1, 0), BubbleGradient.VERTICAL));
+                item.setTypeface(mMediumTypeface);
+
+                 item.setTextColor(ContextCompat.getColor(mBubblePicker.getContext(), android.R.color.white));
+                    return item;
+                }
+            };
+        mBubblePicker.setAdapter(mAdapter);
+
+        colors.recycle();
+
+
+        mBubblePicker.setListener(new BubblePickerListener() {
+            @Override
+            public void onBubbleSelected(@NotNull PickerItem pickerItem) {
+
+            }
+
+            @Override
+            public void onBubbleDeselected(@NotNull PickerItem pickerItem) {
+
+            }
+
         });
-    }
-    public static class TestingTask extends AsyncTask<Void,Void,Void> {
-        private WeakReference<GenreChildTab> mFragment;
-        TestingTask(GenreChildTab tab) {
-            mFragment = new WeakReference<>(tab);
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            if(mFragment.get()!=null) {
-                mFragment.get().test();
-            }
-
-            return null;
-        }
-    }
-
-    public static class UserKey {
-        private final String mUserName;
-        private final int mID;
-
-        public UserKey(String mUserName, int mID) {
-            this.mUserName = mUserName;
-            this.mID = mID;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof UserKey)) return false;
-            return (mUserName).equals(((UserKey) o).mUserName) && mID == ((UserKey) o).mID;
-        }
-
-        @Override
-        public int hashCode() {
-            return mUserName.hashCode();
-        }
     }
 
 }
